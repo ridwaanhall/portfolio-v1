8405f278c5c4ef8964a59e2d13adf0715479cc90
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-09-15 10:30:57 +0000

    modify home to code404 function

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return url_for('error')
+  return redirect(url_for('code_404'))
   #return render_template("portfolio.html")
 
 
-@app.route("/404qweqweqwewqeqw")
-def error():
-  return "404"
+@app.route("/not_found")
+def code_404():
+  return render_template("404.html"), 404
